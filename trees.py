09ac7eec8bd24a2491b584f53eab2f016a53eb70
zengtaotao3390 <378041351@qq.com>
2019-08-22 16:43:36 +0800

    修改参数命名，更贴近现实

--- trees.py
+++ trees.py
@@ -3,11 +3,11 @@ from math import log
 def calcShannonEnt(dataSet):
     numEntities = len(dataSet)
     labelCount = {}
-    for entity in dataSet:
-        entityLabel = entity[-1]
-        if entityLabel not in labelCount.keys():
-            labelCount[entityLabel] = 0
-        labelCount[entityLabel] += 1
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCount.keys():
+            labelCount[currentLabel] = 0
+        labelCount[currentLabel] += 1
     shannonEnt = 0.0
     # 出现该分类的概率
     for label in labelCount.keys():
@@ -29,7 +29,9 @@ def createDataSet():
     return dataSet, labels
 
 
-mydata, labels = createDataSet()
-print(calcShannonEnt(mydata))
-mydata[0][-1] = 'maybe'
-print(calcShannonEnt(mydata))
+# mydata, labels = createDataSet()
+# print(calcShannonEnt(mydata))
+# mydata[0][-1] = 'maybe'
+# print(calcShannonEnt(mydata))
+
+
