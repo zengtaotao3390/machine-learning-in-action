d8d3cf345502be1ae25f873bbc64ca8e1d993260
zengtao <378041351@qq.com>
2019-08-25 16:24:02 +0800

    获取树的深度和宽度

--- treePlotter.py
+++ treePlotter.py
@@ -1,1 +1,59 @@
 import matplotlib.pyplot as plt
+import trees
+
+
+dicidionNode = dict(boxstyle='sawtooth', fc='0.8')
+leafNode = dict(boxstyle='round4', fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+
+
+def plotNode(nodeText, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeText, xy=parentPt, xycoords='axes fraction', xytext=centerPt,
+                            textcoords='axes fraction', va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+def createPlot():
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111, frameon=False)
+    plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), dicidionNode)
+    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+
+# createPlot()
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstKey = list(myTree.keys())[0]
+    secondDict = myTree[firstKey]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstKey = list(myTree.keys())[0]
+    secondDict = myTree[firstKey]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+# dataSet, labels = trees.createDataSet()
+# tree = trees.createTree(dataSet, labels)
+# print(tree)
+# print(getNumLeafs(tree))
+# print(getTreeDepth(tree))
+
+
+
+
+
