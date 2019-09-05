1be3faa0d18ee735165359ba59d092acbb22c3c7
zengtaotao3390 <378041351@qq.com>
2019-09-05 17:45:37 +0800

    logistic<feat>: 画出决策边界

--- logRegres.py
+++ logRegres.py
@@ -1,5 +1,5 @@
-import math
 from numpy import *
+import matplotlib.pyplot as plt
 
 def loadData():
     with open('./machinelearninginaction/Ch05/testSet.txt') as orgFile:
@@ -32,5 +32,30 @@ def gradientAscent(dataSet, classLabel):
     return weights
 
 
-dataSet, classLabel = loadData()
-print(gradientAscent(dataSet, classLabel))
+# dataSet, classLabel = loadData()
+# print(gradientAscent(dataSet, classLabel))
+
+def plotBestFit():
+    aPointX = []; aPointY = []
+    bPointX= []; bPointY = []
+    dataSet, classLabel = loadData()
+    rowNum = shape(dataSet)[0]
+    weights = gradientAscent(dataSet, classLabel).A
+    for i in range(rowNum):
+        if int(classLabel[i]) == 1:
+            aPointX.append(dataSet[i][1])
+            aPointY.append(dataSet[i][2])
+        else:
+            bPointX.append(dataSet[i][1])
+            bPointY.append(dataSet[i][2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(aPointX, aPointY, s=30, c='red', marker='s')
+    ax.scatter(bPointX, bPointY, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    plt.plot(x, y)
+    plt.xlabel('X1');plt.ylabel('X2')
+    plt.show()
+
+plotBestFit()
