367b66a7f4af220def4f46d0b0ff1578d75f49ae
zengtaotao3390 <378041351@qq.com>
2019-09-12 10:37:14 +0800

    SVM<feat>: 序列最小优化函数

---
+++ svmMLiA.py
@@ -0,0 +1,136 @@
+import random
+from numpy import *
+
+
+def loadDataSet(fileName):
+    dataMat = [];
+    labelMat = []
+    with open(fileName) as file:
+        lines = file.readlines()
+        for line in lines:
+            lineArr = line.strip().split('\t')
+            dataMat.append([float(lineArr[0]), float(lineArr[1])])
+            labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+
+def selectJrand(i, m):
+    j = i
+    while j == i:
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if aj < L:
+        aj = L
+    return aj
+
+
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn);
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iter = 0
+    while iter < maxIter:
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                Ej = fXj - labelMat[j]
+                alphaIOld = alphas[i].copy()
+                alphaJOld = alphas[j].copy()
+                if labelMat[i] != labelMat[j]:
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if H == L:
+                    print("L==H")
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - dataMatrix[i, :] * dataMatrix[i, :].T - \
+                      dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta >= 0:
+                    print("eta >= 0")
+                    continue
+                alphas[j] -= labelMat[j] * (Ei - Ej) / eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+                if abs(alphas[j] - alphaJOld) < 0.00001:
+                    print('j not moving enough')
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJOld - alphas[j])
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIOld) * dataMatrix[i, :] * dataMatrix[i, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJOld) * dataMatrix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIOld) * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJOld) * dataMatrix[j, :] * dataMatrix[j, :].T
+                if 0 < alphas[i] < C:
+                    b = b1
+                elif 0 < alphas[j] < C:
+                    b = b2
+                else:
+                     b = (b1 + b2) / 2
+                alphaPairsChanged += 1
+                print('ter: {} i {}, pairs changed {}'.format(iter, i, alphaPairsChanged))
+        if alphaPairsChanged == 0:
+            iter += 1
+        else:
+            iter = 0
+        print('iteration number: {}'.format(iter))
+    return b, alphas
+
+
+def smoSimple1(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
+    b = 0; m,n = shape(dataMatrix)
+    alphas = mat(zeros((m,1)))
+    iter = 0
+    while (iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
+            Ei = fXi - float(labelMat[i])#if checks if an example violates KKT conditions
+            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i,m)
+                fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy(); alphaJold = alphas[j].copy();
+                if (labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L==H: print("L==H"); continue
+                eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T - dataMatrix[j,:]*dataMatrix[j,:].T
+                if eta >= 0: print("eta>=0"); continue
+                alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+                alphas[j] = clipAlpha(alphas[j],H,L)
+                if (abs(alphas[j] - alphaJold) < 0.00001): print("j not moving enough"); continue
+                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])#update i by the same amount as j
+                                                                        #the update is in the oppostie direction
+                b1 = b - Ei- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
+                b2 = b - Ej- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[j,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[j,:]*dataMatrix[j,:].T
+                if (0 < alphas[i]) and (C > alphas[i]): b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]): b = b2
+                else: b = (b1 + b2)/2.0
+                alphaPairsChanged += 1
+                print("iter: {} i:{}, pairs changed {}".format(iter,i,alphaPairsChanged))
+        if (alphaPairsChanged == 0): iter += 1
+        else: iter = 0
+        print("iteration number: {}".format(iter))
+    return b,alphas
+
+dataMatIn, classLabels = loadDataSet('./machinelearninginaction/Ch06/testSet.txt')
+b, alphas = smoSimple(dataMatIn, classLabels, 0.6, 0.001, 40)
+print(alphas[alphas > 0])
+for i in range(100):
+    if alphas[i] > 0.0 :
+        print(dataMatIn[i], classLabels[i])
