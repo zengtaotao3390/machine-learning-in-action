b1400f2305cd37dfb0efb822c352da336ba04403
zengtaotao3390 <378041351@qq.com>
2019-09-20 17:22:49 +0800

    regression<feat>: 局部加权线性回归

--- regression.py
+++ regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName):
     lines = []
@@ -30,4 +31,55 @@ def standRegres(xArr, yArr):
 
 xArr, yArr = loadDataSet('./machinelearninginaction/Ch08/ex0.txt')
 ws = standRegres(xArr, yArr)
-print(ws)
+xMat = np.mat(xArr)
+yMat = np.mat(yArr)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+xCopy = xMat.copy()
+# axis = 0,表示y轴
+xCopy.sort(0)
+print(xCopy)
+yHat = xCopy * ws
+ax.plot(xCopy[:, 1], yHat)
+plt.show()
+print(np.corrcoef((xMat * ws).T, yMat))
+
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye(m))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k**2))
+    xTx = xMat.T * (weights * xMat)
+    if np.linalg.det(xTx) == 0.0:
+        print('This matrix is singular, cannot do inverse')
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+# print(yArr[0])
+# print(lwlr(xArr[0], xArr, yArr, 1.0))
+# print(lwlr(xArr[0], xArr, yArr, 0.001))
+# print(np.mat(xMat))
+yHat = lwlrTest(xArr, xArr, yArr, 1.0)
+xMat = np.mat(xArr)
+srtInd = xMat[:, 1].argsort(0)
+xSort = xMat[srtInd][:, 0, :]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(xSort[:, 1], yHat[srtInd])
+ax.scatter(xMat[:, 1].flatten().A[0], np.mat(yArr).T.flatten().A[0], s=2, c='red')
+plt.show()
