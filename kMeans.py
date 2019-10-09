228ee43a9d1d4b0b636cfeaebec448d7809dee1e
zengtaotao3390 <378041351@qq.com>
2019-10-09 12:35:15 +0800

    kMeans<feat>: 完成训练

--- kMeans.py
+++ kMeans.py
@@ -16,7 +16,7 @@ def loadDataSet(fileName):
 def disEclud(vecA, vecB):
     return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
 
-
+# 最大最小之间的随机数
 def randCent(dataSet, k):
     n = np.shape(dataSet)[1]
     centroids = np.mat(np.ones((k, n)))
@@ -32,5 +32,63 @@ dataMat = np.mat(loadDataSet('./machinelearninginaction/Ch10/testSet.txt'))
 # print(max(dataMat[:, 0]))
 # print(min(dataMat[:, 1]))
 # print(max(dataMat[:, 1]))
-print(randCent(dataMat, 2))
-print(disEclud(dataMat[0], dataMat[1]))
+# print(randCent(dataMat, 2))
+# print(disEclud(dataMat[0], dataMat[1]))
+
+
+def kMeans(dataSet, k, distMeans=disEclud, createCent=randCent):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = np.inf
+            minIndex = -1
+            for j in range(k):
+                distJI = distMeans(centroids[j, :], dataSet[i, :])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist**2
+        print(centroids)
+        for cent in range(k):
+            ptsInCluster = dataSet[np.nonzero(clusterAssment[:, 0].A==cent)[0]]
+            centroids[cent, :] = np.mean(ptsInCluster, axis=0)
+    return centroids, clusterAssment
+
+
+
+def kMeans1(dataSet, k, distMeas=disEclud, createCent=randCent):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m,2)))#create mat to assign data points
+                                      #to a centroid, also holds SE of each point
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):#for each data point assign it to the closest centroid
+            minDist = np.inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i,0] != minIndex: clusterChanged = True
+            clusterAssment[i,:] = minIndex,minDist**2
+        print(centroids)
+        for cent in range(k):#recalculate centroids
+            ptsInClust = dataSet[np.nonzero(clusterAssment[:,0].A==cent)[0]]#get all the point in this cluster
+            centroids[cent,:] = np.mean(ptsInClust, axis=0) #assign centroid to mean
+    return centroids, clusterAssment
+
+myCentroids, clustAssing = kMeans(dataMat, 4)
+
+
+
+
+
+
+
